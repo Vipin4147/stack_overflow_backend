7052f95bb3e0939f5b1669a1fa1309a0412b35d8
Vipin4147 <vipin4147@gmail.com>
2023-05-09 18:49:38 +0530

    dplmnt

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.get("/", () => {
+app.get("/", (req, res) => {
   res.send("HOME PAGE");
 });
 app.use("/", userRouter);
